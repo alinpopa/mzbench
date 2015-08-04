d06228d96391d080933f3bfb8f4e2879633cf8e9
Timofey Barmin <tbarmin@machinezone.com>
2015-08-03 17:16:04 -0700

    do_stuff function in dummy_worker

--- node/apps/dummy_worker/src/dummy_worker.erl
+++ node/apps/dummy_worker/src/dummy_worker.erl
@@ -1,14 +1,14 @@
 -module(dummy_worker).
 
 -export([initial_state/0, metrics/0,
-         print/3, test_method/3]).
+         print/3, test_method/3, do_stuff/2, do_stuff2/2]).
 
 -type state() :: string().
 
 -spec initial_state() -> state().
-initial_state() -> "".
+initial_state() -> lists:seq(0, 10000).
 
-metrics() -> [[{"print", counter}, {"print_2", counter}], {"dummy", histogram}].
+metrics() -> [[{"print", counter}, {"print_2", counter}], {"dummy", histogram}, {"stuff", counter}].
 
 print(State, Meta, Text) ->
     _ = mzb_metrics:notify("print", 1),
@@ -17,6 +17,23 @@ print(State, Meta, Text) ->
     _ = mzb_metrics:notify({"dummy", histogram}, N/7),
     lager:info("Appending ~p, Meta: ~p~n", [Text, Meta]),
     {nil, State}.
+
+% just load cpu with smth
+do_stuff(State, _Meta) ->
+    N = random:uniform(1000000),
+    _ = mzb_metrics:notify({"stuff", counter}, 1),
+    _ = mzb_metrics:notify({"dummy", histogram}, N),
+    case State of
+        [_|Tail] when length(State) > 10000 -> {nil, Tail ++ [N]};
+        _ -> {nil, [N|State]}
+    end.
+
+do_stuff2(State, _Meta) ->
+    N = random:uniform(1000000),
+    case State of
+        [_|Tail] when length(State) > 10000 -> {nil, Tail ++ [N]};
+        _ -> {nil, [N|State]}
+    end.
 
 test_method(State, _Meta, Text) ->
     {nil, Text ++ State}.
